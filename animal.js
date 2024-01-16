4e2c151ce4013bfe2659e5f1756693e517c48b5d
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-01-16 11:52:42 +0100

    another file
    added another file and made some changes on the emoji

---
+++ animal.js
@@ -0,0 +1,67 @@
+//background
+function setup() {
+  createCanvas(800, 1000);
+}
+
+let blackColor = color(0, 0, 0);
+
+let whiteColor = color(255, 255, 255);
+
+let x = 200;
+let y = 200;
+let width = 200;
+let height = 200;
+
+function draw() {
+  background(40, 100, 20);
+
+  //arms
+  noStroke();
+  fill(blackColor);
+  rect(x - 190, y + 140, width + 100, height - 145, 100);
+  rect(x + 130, y + 140, width + 100, height - 145, 100);
+
+  //legs
+  fill(blackColor);
+  rect(x + 20, y + 310, width - 135, height, 100);
+  rect(x + 130, y + 310, width - 135, height, 100);
+
+  //body
+  fill(whiteColor);
+  ellipse(x + 110, y + 240, width + 100);
+
+  //belly
+  fill(blackColor);
+  ellipse(x + 110, y + 240, width - 40, height);
+
+  //ears
+  fill(blackColor);
+  ellipse(x, y - 100, width - 100);
+  ellipse(x + 200, y - 100, width - 100);
+
+  //head
+  fill(whiteColor);
+  ellipse(x + 100, y, width);
+
+  //eyes
+  fill(blackColor);
+  ellipse(x + 60, y - 20, width - 140);
+  ellipse(x + 140, y - 20, width - 140);
+
+  //pupils
+  noFill();
+  stroke(whiteColor);
+  strokeWeight(6);
+  ellipse(x + 60, y - 20, width - 180);
+  ellipse(x + 140, y - 20, width - 180);
+
+  //mouth
+  fill(blackColor);
+  noStroke();
+  ellipse(x + 100, y + 40, width - 145);
+
+  noFill();
+  stroke(whiteColor);
+  strokeWeight(2);
+  ellipse(x + 100, y + 30, width - 175, height - 184);
+}
--- emoji.js
+++ emoji.js
@@ -4,26 +4,12 @@
  */
 
 //background
-background(50, 50, 50);
+background(25, 50, 0);
 
 //head
 strokeWeight(2.5);
 fill(255, 255, 0);
 ellipse(200, 200, 300);
-
-//right eye and pupil
-fill(255, 255, 255);
-ellipse(270, 150, 80);
-
-fill(0, 0, 0);
-ellipse(250, 150, 20);
-
-//left eye and pupil
-fill(255, 255, 255);
-ellipse(150, 150, 120);
-
-fill(0, 0, 0);
-ellipse(170, 150, 40);
 
 //mouth and teeth
 fill(255, 255, 255);
@@ -33,9 +19,11 @@ stroke(0, 0, 0);
 line(137, 240, 273, 240);
 line(134, 280, 275, 280);
 
-//fill(0, 0, 0);
-//beginShape();
-//vertex(100, 100);
-//bezierVertex(80, 160, 90, 160, 160, 40);
-//endShape();
-//arc(210, 250, 150, 100, 0, PI);
+//eyes and pupils
+fill(255, 255, 255);
+ellipse(270, 150, 80);
+ellipse(150, 150, 120);
+
+fill(0, 0, 0);
+ellipse(250, 150, 20);
+ellipse(170, 150, 40);
