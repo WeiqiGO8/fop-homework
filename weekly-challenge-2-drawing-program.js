a7c241d9d25d64947bcf2979c7fec7cca78bf027
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-01-25 14:06:42 +0100

    update
    added color picker and fixed the buttons

--- weekly-challenge-2-drawing-program.js
+++ weekly-challenge-2-drawing-program.js
@@ -1,25 +1,56 @@
 frameRate(60);
-// background(255, 255, 255);
 background(0, 0, 0);
+
 let lastMouseX = 0;
 let lastMouseY = 0;
 let mouseIsActive = false;
 
-let x = 160;
-let y = 200;
+let brushShape = "line";
+let brushColor = "mouseColoring";
 
-//buttons to change between line and shape to draw
-function lineTool() {
+//buttons to change brush color
+function mouseColor(x, y) {
+  noStroke();
+  fill(155, 20, 230);
+  rect(x + 350, y + 50, 40, 40);
+}
+
+function redColor(x, y) {
+  noStroke();
+  fill(255, 0, 0);
+  rect(x + 350, y + 100, 40, 40);
+}
+
+function greenColor(x, y) {
+  noStroke();
+  fill(0, 255, 0);
+  rect(x + 350, y + 150, 40, 40);
+}
+
+function blueColor(x, y) {
+  noStroke();
+  fill(0, 0, 255);
+  rect(x + 350, y + 200, 40, 40);
+}
+
+function whiteColor(x, y) {
+  noStroke();
+  fill(255, 255, 255);
+  rect(x + 350, y + 250, 40, 40);
+}
+
+//buttons to change brush shape
+function lineTool(x, y) {
   noStroke();
   fill(255, 255, 255);
   rect(x - 110, y + 300, 100, 100);
 
   stroke(0, 0, 0);
   strokeWeight(4);
-  line(x - 90, y + 350, 130, 550);
+  line(x - 100, y + 350, 180, 550);
 }
 
-function squareToolOne() {
+function squareTool(x, y) {
   noStroke();
   fill(255, 255, 255);
   rect(x, y + 300, 100, 100);
@@ -28,54 +59,163 @@ function squareToolOne() {
   rect(x + 10, y + 310, 80, 80);
 }
 
-function squareToolTwo() {
+function circleTool(x, y) {
   noStroke();
   fill(255, 255, 255);
   rect(x + 110, y + 300, 100, 100);
 
   fill(0, 0, 0);
-  rect(x + 120, y + 340, 80, 20);
+  ellipse(x + 160, y + 350, 60);
 }
 
-function circleTool() {
+function clearCanvas(x, y) {
   noStroke();
   fill(255, 255, 255);
   rect(x + 220, y + 300, 100, 100);
 
   fill(0, 0, 0);
-  ellipse(x + 270, y + 350, 60);
-}
-
-function clearCanvas() {
-  noStroke();
-  fill(255, 255, 255);
-  rect(x + 330, y + 300, 100, 100);
-
-  fill(0, 0, 0);
   strokeWeight();
-  text("clear canvas", x + 345, y + 355);
+  text("Clear Canvas", x + 235, y + 355);
 }
 
-lineTool();
-squareToolOne();
-squareToolTwo();
-circleTool();
-clearCanvas();
+function mouseClicked() {
+  console.log("The mouse was clicked");
+  if (mouseX > 90 && mouseX < 90 + 100 && mouseY > 500 && mouseY < 500 + 100) {
+    console.log("you are drawing a line!");
+    brushShape = "line";
+  } else if (
+    mouseX > 200 &&
+    mouseX < 200 + 100 &&
+    mouseY > 500 &&
+    mouseY < 500 + 100
+  ) {
+    console.log("you are drawing a square!");
+    brushShape = "square";
+  } else if (
+    mouseX > 310 &&
+    mouseX < 310 + 100 &&
+    mouseY > 500 &&
+    mouseY < 500 + 100
+  ) {
+    console.log("you are drawing a circle!");
+    brushShape = "circle";
+  } else if (
+    mouseX > 420 &&
+    mouseX < 420 + 100 &&
+    mouseY > 500 &&
+    mouseY < 500 + 100
+  ) {
+    console.log("you have cleared the canvas");
+    brushShape = "cleared";
+  } else if (
+    mouseX > 550 &&
+    mouseX < 550 + 40 &&
+    mouseY > 250 &&
+    mouseY < 250 + 40
+  ) {
+    console.log("mouse color");
+    brushColor = "mouseColoring";
+  } else if (
+    mouseX > 550 &&
+    mouseX < 550 + 40 &&
+    mouseY > 300 &&
+    mouseY < 300 + 40
+  ) {
+    console.log("color red");
+    brushColor = "redColoring";
+  } else if (
+    mouseX > 550 &&
+    mouseX < 550 + 40 &&
+    mouseY > 350 &&
+    mouseY < 350 + 40
+  ) {
+    console.log("color green");
+    brushColor = "greenColoring";
+  } else if (
+    mouseX > 550 &&
+    mouseX < 550 + 40 &&
+    mouseY > 400 &&
+    mouseY < 400 + 40
+  ) {
+    console.log("color blue");
+    brushColor = "blueColoring";
+  } else if (
+    mouseX > 550 &&
+    mouseX < 550 + 40 &&
+    mouseY > 450 &&
+    mouseY < 450 + 40
+  ) {
+    console.log("color white");
+    brushColor = "whiteColoring";
+  }
+}
 
 function draw() {
   strokeWeight(4);
   if (mouseIsPressed) {
     if (mouseIsActive) {
-      stroke(lastMouseX, 100, lastMouseY);
-      line(lastMouseX, lastMouseY, mouseX, mouseY);
-    }
+      if (brushColor === "mouseColoring") {
+        stroke(lastMouseX, 100, lastMouseY);
+      } else if (brushColor === "redColoring") {
+        stroke(255, 0, 0);
+      } else if (brushColor === "greenColoring") {
+        stroke(0, 255, 0);
+      } else if (brushColor === "blueColoring") {
+        stroke(0, 0, 255);
+      } else if (brushColor === "whiteColoring") {
+        stroke(255, 255, 255);
+      }
+      if (brushShape === "line") {
+        line(lastMouseX, lastMouseY, mouseX, mouseY);
+      } else if (brushShape === "square") {
+        if (brushColor === "mouseColoring") {
+          fill(lastMouseX, 100, lastMouseY);
+        } else if (brushColor === "redColoring") {
+          fill(255, 0, 0);
+        } else if (brushColor === "greenColoring") {
+          fill(0, 255, 0);
+        } else if (brushColor === "blueColoring") {
+          fill(0, 0, 255);
+        } else if (brushColor === "whiteColoring") {
+          fill(255, 255, 255);
+        }
 
+        rect(mouseX, mouseY, 80, 80);
+      } else if (brushShape === "circle") {
+        if (brushColor === "mouseColoring") {
+          fill(lastMouseX, 100, lastMouseY);
+        } else if (brushColor === "redColoring") {
+          fill(255, 0, 0);
+        } else if (brushColor === "greenColoring") {
+          fill(0, 255, 0);
+        } else if (brushColor === "blueColoring") {
+          fill(0, 0, 255);
+        } else if (brushColor === "whiteColoring") {
+          fill(255, 255, 255);
+        }
+
+        ellipse(mouseX, mouseY, 60);
+      } else if (brushShape === "cleared") {
+        clear();
+        background(0, 0, 0);
+      }
+    }
     lastMouseX = mouseX;
     lastMouseY = mouseY;
     mouseIsActive = true;
   } else {
     mouseIsActive = false;
   }
+
+  lineTool(200, 200);
+  squareTool(200, 200);
+  circleTool(200, 200);
+  clearCanvas(200, 200);
+  redColor(200, 200);
+  greenColor(200, 200);
+  blueColor(200, 200);
+  whiteColor(200, 200);
+  mouseColor(200, 200);
 }
 
 function mousePressed() {
@@ -84,8 +224,4 @@ function mousePressed() {
 
 function mouseReleased() {
   console.log("The mouse was released");
-}
-
-function mouseClicked() {
-  console.log("The mouse was clicked");
 }
