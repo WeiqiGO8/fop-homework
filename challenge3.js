72e78a1632268bb97dbfadd4ca2c386f2f5239cd
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-02-01 14:00:57 +0100

    update - created an object for a for loop to go through to read the different values

--- challenge3.js
+++ challenge3.js
@@ -1,30 +1,27 @@
-let snowX = [];
-let snowY = [];
-let snowAlpha = [];
-let snowMovementX = [];
-let snowMovementY = [];
-let snowAcceleration = [];
+let snowing = [];
 
 for (let i = 0; i < 200; i++) {
-  let x = Math.floor(Math.random() * width);
-  let fallX = Math.floor(Math.random() * width);
-  let y = Math.floor(Math.random() * height);
-  let fallY = Math.floor(Math.random() * height);
-  let alpha = Math.random();
-  let acceleration = 0.4;
+  let snow = {
+    x: Math.floor(Math.random() * width),
+    y: Math.floor(Math.random() * height),
+    alpha: Math.random(),
+    velocity: Math.floor(Math.random()),
+    acceleration: Math.floor(Math.random()),
+  };
 
-  snowX.push(x);
-  snowY.push(y);
-  snowAlpha.push(alpha);
+  snowing.push(snow);
 }
 
 function draw() {
   noStroke();
   background(0, 0, 0);
 
-  for (let index in snowX) {
-    fill(255, 255, 255, Math.abs(Math.sin(snowAlpha[index])) * 255);
-    ellipse(snowX[index], snowY[index], 6);
-    snowAlpha += 0.4;
+  for (let snow of snowing) {
+    fill(255, 255, 255, Math.abs(Math.sin(snow.alpha)) * 255);
+    ellipse(snow.x, snow.y, 6);
+    snow.alpha += 0.1;
+    console.log(snow.alpha);
+    // for (let m in snowY) {
+    // }
   }
 }
