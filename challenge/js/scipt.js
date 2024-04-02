f474f840bd333041e5764ec17ef761a2f4b07a12
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-04-02 17:18:11 +0200

    challenge 4 - dynamic portfolio

---
+++ challenge/js/scipt.js
@@ -0,0 +1,27 @@
+// objects
+let portfolioItems = {
+  img: image1,
+  src: img,
+  alt: imagetext,
+  title: hello,
+  description: desc,
+};
+
+//arrays
+let portfolioItemsElement = [];
+
+// for (let portfolioItemElement of portfolioItemsElement) {
+//   const portfolioItemsElement = document.createElement("figure");
+//   portfolioItemsElement.innerText = portfolioItems;
+//   portfolioItemsElement.classList.add("figure-style");
+//   portfolioItems.push(portfolioItems);
+//   console.log(portfolioItems);
+// }
+
+function addPortfolioContent() {
+  const portfolioFigureElement = document.createElement("figure");
+  const portfolioaElement = document.createElement("a");
+  portfolioaElement.innerText = "#";
+  const portfolioImgEElement = documnet.createElement("img");
+  let portfolioFigcaptionElement = document.createElement("figcaption");
+}
