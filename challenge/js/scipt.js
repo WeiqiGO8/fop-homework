4fb74be68d0d7f7b14ad7f428c8b644c6e73a335
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-04-03 21:36:01 +0200

    worked out some of the logic for the challenge

--- challenge/js/scipt.js
+++ challenge/js/scipt.js
@@ -1,27 +1,51 @@
 // objects
-let portfolioItems = {
-  image: "img",
-  src: "img/rock-1.jpg",
-  alt: "img text",
-  title: "hello",
-  description: "desc",
+const contentIdElement = document.getElementById("content");
+
+let project1 = {
+  id: 1,
+  imgSrc: "img/rock-1.jpg",
+  imgAlt: "A image of a rock",
+  projectTitle: "project 1",
+  projectDesc: "description of project 1",
 };
 
 //arrays
-let portfolioItemsElement = [];
+let contentProjectElement = [];
 
-// for (let portfolioItemElement of portfolioItemsElement) {
-//   const portfolioItemsElement = document.createElement("figure");
-//   portfolioItemsElement.innerText = portfolioItems;
-//   portfolioItemsElement.classList.add("figure-style");
-//   portfolioItems.push(portfolioItems);
-//   console.log(portfolioItems);
-// }
+contentProjectElement.push(project1);
 
-function addPortfolioContent() {
-  const portfolioFigureElement = document.createElement("figure");
-  const portfolioaElement = document.createElement("a");
-  portfolioaElement.innerText = "#";
-  const portfolioImgEElement = documnet.createElement("img");
-  let portfolioFigcaptionElement = document.createElement("figcaption");
+function createProjectElement(project) {
+  const projectFigureElement = document.createElement("figure");
+  projectFigureElement.classList.add("project-post");
+  projectFigureElement.id = project.id;
+
+  const projectaElement = document.createElement("a");
+  projectaElement.href = "#";
+
+  const projectImgElement = document.createElement("img");
+  projectImgElement.src = project.imgSrc;
+  projectImgElement.alt = project.imgAlt;
+
+  projectaElement.appendChild(projectImgElement);
+
+  const projectFigcapTitleElement = document.createElement("figcaption");
+  projectFigcapTitleElement.innerText = project.projectTitle;
+
+  const projectFigcapDescElement = document.createElement("figcaption");
+  projectFigcapDescElement.innerText = project.projectDesc;
+
+  projectaElement.appendChild(projectFigcapTitleElement);
+  projectaElement.appendChild(projectFigcapDescElement);
+
+  projectFigureElement.appendChild(projectaElement);
+  return projectFigureElement;
 }
+
+const projectElement = createProjectElement(project1);
+contentIdElement.appendChild(projectElement);
+
+//The following 4 lines of code was adapted from ChatGPT: https://chat.openai.com/share/7964f237-673a-41ef-a93d-2ed44f753bc1 - 2024-04-03
+contentProjectElement.forEach((project) => {
+  const projectElement = createProjectElement(project);
+  contentIdElement.appendChild(projectElement);
+});
