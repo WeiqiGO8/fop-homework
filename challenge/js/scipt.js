326e15f6bd327e868950097797846b09c99c36ac
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-04-04 10:24:59 +0200

    update image hamtergram

--- challenge/js/scipt.js
+++ challenge/js/scipt.js
@@ -1,18 +1,19 @@
 // objects
 const contentIdElement = document.getElementById("content");
 
-let project1 = {
-  id: 1,
-  imgSrc: "img/rock-1.jpg",
-  imgAlt: "A image of a rock",
-  projectTitle: "project 1",
-  projectDesc: "description of project 1",
-};
-
 //arrays
 let contentProjectElement = [];
 
-contentProjectElement.push(project1);
+for (let i = 1; i <= 12; i++) {
+  let project = {
+    id: i,
+    imgSrc: `img/project-${i}.jpg`,
+    imgAlt: `An image of project ${i}`,
+    projectTitle: `project ${i}`,
+    projectDesc: `description of project ${i}`,
+  };
+  contentProjectElement.push(project);
+}
 
 function createProjectElement(project) {
   const projectFigureElement = document.createElement("figure");
@@ -40,8 +41,6 @@ function createProjectElement(project) {
   projectFigureElement.appendChild(projectaElement);
   return projectFigureElement;
 }
-
-const projectElement = createProjectElement(project1);
 
 //The following 4 lines of code was adapted from ChatGPT: https://chat.openai.com/share/7964f237-673a-41ef-a93d-2ed44f753bc1 - 2024-04-03
 contentProjectElement.forEach((project) => {
